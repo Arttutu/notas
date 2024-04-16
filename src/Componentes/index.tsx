91e4197c2eac2cd39a7015fe6b4e78d64bff7ca8
Arttutu <arthurtt08@gmail.com>
2024-04-16 10:10:38 -0300

    terminando estilo modal notas

--- src/Componentes/index.tsx
+++ src/Componentes/index.tsx
@@ -5,26 +5,30 @@ export default function Form() {
 <div className='mt-4'>
   <Dialog.Root >
     <Dialog.Trigger>
-      <div className='border-black border-[1px] p-[4px]  rounded-md'>
+      <div className='border-blue-900 border-[1px] p-[4px]  rounded-md'>
         <button className='text-blue-900 text-sm font-semibold '>Adicione uma nota +</button>
       </div>
     </Dialog.Trigger>
     <Dialog.Portal>
-      <Dialog.Overlay className="fixed inset-0 bg-black/50 flex justify-center items-center" />
-      <Dialog.Content className="h-[500px] w-[350px] sm:w-[744px]  bg-white ">
-        <form>
-          <label>Titulo</label>
-          <input></input>
-          <label>Descrição</label>
-          <input></input>
-          <label>Data</label>
-          <input></input>
+      <Dialog.Overlay className=" fixed inset-0 bg-black/50 flex items-center" />
+      <Dialog.Content className="w-full  max-w-[600px] bg-white  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg absolute p-4 ">
+        <form className='flex flex-col items-start gap-2'>
+          <label  className='font-semibold text-blue-900'>Titulo</label>
+          <input className='w-full p-1 border border-gray-300 rounded-md'></input>
+          <label className='font-semibold text-blue-900' >Descrição</label>
+          <input  className='w-full p-1 border border-gray-300 rounded-md '></input>
+          <label className='font-semibold text-blue-900'>Data</label>
+          <input className='w-full p-1 border border-gray-300 rounded-md' ></input>
+          <div className='border-blue-900 border-[1px] p-[4px]  rounded-md mt-2'>
+            <button className='text-blue-900 text-sm font-bold '>Adicionar +</button>
+          </div>
         </form>
         <Dialog.Close />
       </Dialog.Content>
     </Dialog.Portal>
   </Dialog.Root>
-</div>
+</div> 
 
   )
 }
+ 
